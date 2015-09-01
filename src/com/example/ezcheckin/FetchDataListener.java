c4c1b194e1c7c10d9165a45cb299839b9df0b721
Girish Raman <rgirish1994@gmail.com>
2015-09-01 12:24:29 -0700

    Removed all files

--- src/com/example/ezcheckin/FetchDataListener.java
+++
@@ -1,8 +0,0 @@
-package com.example.ezcheckin;
-
-public interface FetchDataListener 
-{
-	public void onFetchComplete();
-    public void onFetchFailure(String msg);
-	public void onFetchComplete(String string);
-}
